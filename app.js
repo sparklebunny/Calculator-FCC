ec50e522ecaf98782ffde667939e27b6c3e0c366
JeriBeth Stevens <jeribeth@gmail.com>
2017-02-05 17:47:18 -0500

    committing local files for calculator project

---
+++ app.js
@@ -0,0 +1,20 @@
+$document.ready(function){
+
+// set variables for keys and operators
+var keys = document.querySelectorAll("#calculator-body button");
+var operators = ["+", "-", "x", "÷"];
+var decimalAdded = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//FIN
